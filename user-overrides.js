670c3ab6e6e26bd6017cba8cdad06f7b64356a1a
nastavoi <61883414+nastavoi@users.noreply.github.com>
2021-06-07 20:23:08 +0200

    setup-web

--- user-overrides.js
+++ user-overrides.js
@@ -46,11 +46,99 @@ user_pref("browser.download.manager.addToRecentDocs", true);
  * file, including the name, origin, size and a cryptographic hash of the contents, to the Google
  * Safe Browsing service which helps Firefox determine whether or not the file should be blocked
  * [SETUP-SECURITY] If you do not understand this, or you want this protection, then override it ***/
-  // user_pref("browser.safebrowsing.downloads.remote.enabled", false);
+user_pref("browser.safebrowsing.downloads.remote.enabled", true);
   // user_pref("browser.safebrowsing.downloads.remote.url", "");
 /* 0413: disable SB checks for unwanted software
  * [SETTING] Privacy & Security>Security>... "Warn you about unwanted and uncommon software" ***/
 user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", true);
 user_pref("browser.safebrowsing.downloads.remote.block_uncommon", true);
+
+/* 0210: set preferred language for displaying web pages
+ * [TEST] https://addons.mozilla.org/about ***/
+user_pref("intl.accept_languages", "pl, en-US, en");
+/* 0211: enforce US English locale regardless of the system locale
+ * [SETUP-WEB] May break some input methods e.g xim/ibus for CJK languages [1]
+ * [1] https://bugzilla.mozilla.org/buglist.cgi?bug_id=867501,1629630 ***/
+user_pref("javascript.use_us_english_locale", false); // [HIDDEN PREF]
+
+/* 0860: disable search and form history
+ * [SETUP-WEB] Be aware that autocomplete form data can be read by third parties [1][2]
+ * [NOTE] We also clear formdata on exit (see 2803)
+ * [SETTING] Privacy & Security>History>Custom Settings>Remember search and form history
+ * [1] https://blog.mindedsecurity.com/2011/10/autocompleteagain.html
+ * [2] https://bugzilla.mozilla.org/381681 ***/
+user_pref("browser.formfill.enable", true);
+
+/* 1223: enforce strict pinning
+ * PKP (Public Key Pinning) 0=disabled 1=allow user MiTM (such as your antivirus), 2=strict
+ * [SETUP-WEB] If you rely on an AV (antivirus) to protect your web browsing
+ * by inspecting ALL your web traffic, then leave at current default=1
+ * [1] https://gitlab.torproject.org/tpo/applications/tor-browser/-/issues/16206 ***/
+user_pref("security.cert_pinning.enforcement_level", 1);
+
+/* override recipe: enable DRM and let me watch videos ***/
+   // user_pref("media.gmp-widevinecdm.enabled", true); // 1825 default commented out in user.js v86+
+user_pref("media.eme.enabled", true); // 1830
+
+/* 2302: disable service workers [FF32, FF44-compat]
+ * Service workers essentially act as proxy servers that sit between web apps, and the
+ * browser and network, are event driven, and can control the web page/site it is associated
+ * with, intercepting and modifying navigation and resource requests, and caching resources.
+ * [NOTE] Service worker APIs are hidden (in Firefox) and cannot be used when in PB mode.
+ * [NOTE] Service workers only run over HTTPS. Service workers have no DOM access.
+ * [SETUP-WEB] Disabling service workers will break some sites. This pref is required true for
+ * service worker notifications (2304), push notifications (disabled, 2305) and service worker
+ * cache (2740). If you enable this pref, then check those settings as well ***/
+user_pref("dom.serviceWorkers.enabled", true);
+/* 2304: disable Web Notifications
+ * [NOTE] Web Notifications can also use service workers (2302) and are behind a prompt (2306)
+ * [1] https://developer.mozilla.org/docs/Web/API/Notifications_API ***/
+user_pref("dom.webnotifications.enabled", true); // [FF22+]
+user_pref("dom.webnotifications.serviceworker.enabled", true); // [FF44+]
+/* 2305: disable Push Notifications [FF44+]
+ * Push is an API that allows websites to send you (subscribed) messages even when the site
+ * isn't loaded, by pushing messages to your userAgentID through Mozilla's Push Server.
+ * [NOTE] Push requires service workers (2302) to subscribe to and display, and is behind
+ * a prompt (2306). Disabling service workers alone doesn't stop Firefox polling the
+ * Mozilla Push Server. To remove all subscriptions, reset your userAgentID (in about:config
+ * or on start), and you will get a new one within a few seconds.
+ * [1] https://support.mozilla.org/en-US/kb/push-notifications-firefox
+ * [2] https://developer.mozilla.org/en-US/docs/Web/API/Push_API ***/
+user_pref("dom.push.enabled", true);
+   // user_pref("dom.push.userAgentID", "");
+/* 2306: set a default permission for Notifications (both 2304 and 2305) [FF58+]
+ * 0=always ask (default), 1=allow, 2=block
+ * [NOTE] Best left at default "always ask", fingerprintable via Permissions API
+ * [SETTING] to add site exceptions: Ctrl+I>Permissions>Receive Notifications
+ * [SETTING] to manage site exceptions: Options>Privacy & Security>Permissions>Notifications>Settings ***/
+user_pref("permissions.default.desktop-notification", 0);
+
+/* 2740: disable service worker cache and cache storage
+ * [NOTE] We clear service worker cache on exiting Firefox (see 2803)
+ * [1] https://w3c.github.io/ServiceWorker/#privacy ***/
+user_pref("dom.caches.enabled", true);
+
+/* 4001: enable First Party Isolation [FF51+]
+ * [SETUP-WEB] May break cross-domain logins and site functionality until perfected
+ * [1] https://bugzilla.mozilla.org/buglist.cgi?bug_id=1260931,1299996 ***/
+user_pref("privacy.firstparty.isolate", false);
+
+/* 4501: enable privacy.resistFingerprinting [FF41+]
+ * This pref is the master switch for all other privacy.resist* prefs unless stated
+ * [SETUP-WEB] RFP can cause the odd website to break in strange ways, and has a few side affects,
+ * but is largely robust nowadays. Give it a try. Your choice. Also see 4504 (letterboxing).
+ * [1] https://bugzilla.mozilla.org/418986 ***/
+user_pref("privacy.resistFingerprinting", false);
+
+/* 4504: enable RFP letterboxing [FF67+]
+ * Dynamically resizes the inner window by applying margins in stepped ranges [2]
+ * If you use the dimension pref, then it will only apply those resolutions. The format is
+ * "width1xheight1, width2xheight2, ..." (e.g. "800x600, 1000x1000, 1600x900")
+ * [SETUP-WEB] This does NOT require RFP (see 4501) **for now**, so if you're not using 4501, or you are but
+ * dislike margins being applied, then flip this pref, keeping in mind that it is effectively fingerprintable
+ * [WARNING] The dimension pref is only meant for testing, and we recommend you DO NOT USE it
+ * [1] https://bugzilla.mozilla.org/1407366
+ * [2] https://hg.mozilla.org/mozilla-central/rev/6d2d7856e468#l2.32 ***/
+user_pref("privacy.resistFingerprinting.letterboxing", false); // [HIDDEN PREF]
 
 user_pref("_user.js.parrot", "overrides section successful");
