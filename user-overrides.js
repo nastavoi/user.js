8557016ec52df9fdd8bfdf954df25c50dc38fc69
nastavoi <61883414+nastavoi@users.noreply.github.com>
2021-06-07 19:41:23 +0200

    4th commit

--- user-overrides.js
+++ user-overrides.js
@@ -1,2 +1,43 @@
 /*** MY OVERRIDES ***/
 user_pref("_user.js.parrot", "overrides section syntax error");
+
+/* override recipe: enable session restore ***/
+user_pref("browser.startup.page", 3); // 0102
+  // user_pref("browser.privatebrowsing.autostart", false); // 0110 required if you had it set as true
+  // user_pref("places.history.enabled", true); // 0862 required if you had it set as false
+  // user_pref("browser.sessionstore.privacy_level", 0); // 1021 optional [to restore cookies/formdata]
+  // user_pref("privacy.clearOnShutdown.history", false); // 2803
+  // user_pref("privacy.clearOnShutdown.cookies", false); // 2803 optional
+  // user_pref("privacy.clearOnShutdown.formdata", false); // 2803 optional
+  // user_pref("privacy.cpd.history", false); // 2804 to match when you use Ctrl-Shift-Del
+  // user_pref("privacy.cpd.cookies", false); // 2804 optional
+  // user_pref("privacy.cpd.formdata", false); // 2804 optional
+
+/* 0807: disable live search suggestions
+/* [NOTE] Both must be true for the location bar to work
+ * [SETUP-CHROME] Change these if you trust and use a privacy respecting search engine
+ * [SETTING] Search>Provide search suggestions | Show search suggestions in address bar results ***/
+user_pref("browser.search.suggest.enabled", true);
+user_pref("browser.urlbar.suggest.searches", true);
+
+/* 1023: set the minimum interval between session save operations
+ * Increasing this can help on older machines and some websites, as well as reducing writes [1]
+ * Default is 15000 (15 secs). Try 30000 (30 secs), 60000 (1 min) etc
+ * [SETUP-CHROME] This can also affect entries in the "Recently Closed Tabs" feature:
+ * i.e. the longer the interval the more chance a quick tab open/close won't be captured.
+ * This longer interval *may* affect history but we cannot replicate any history not recorded
+ * [1] https://bugzilla.mozilla.org/1304389 ***/
+user_pref("browser.sessionstore.interval", 15000);
+
+/* 2650: discourage downloading to desktop
+ * 0=desktop, 1=downloads (default), 2=last used
+ * [SETTING] To set your default "downloads": General>Downloads>Save files to ***/
+user_pref("browser.download.folderList", 0);
+/* 2652: disable adding downloads to the system's "recent documents" list ***/
+user_pref("browser.download.manager.addToRecentDocs", true);
+
+/* 2802: enable Firefox to clear items on shutdown (see 2803)
+ * [SETTING] Privacy & Security>History>Custom Settings>Clear history when Firefox closes ***/
+user_pref("privacy.sanitize.sanitizeOnShutdown", false);
+
+user_pref("_user.js.parrot", "overrides section successful");
